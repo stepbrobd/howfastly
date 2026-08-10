0614a4bc7e9baabb5dd06021ac9834cb8fc1d910
Yifei Sun <ysun@hey.com>
2026-08-10 18:02:42 +0200

    pkgs/howfastly: expose web as a passthru

--- pkgs/howfastly/default.nix
+++ pkgs/howfastly/default.nix
@@ -1,5 +1,11 @@
-{ inputs, stdenv }:
+{ callPackage, inputs, lib, pkgsFinal, stdenv }:
 
-inputs.self.legacyPackages.${stdenv.hostPlatform.system}.crates.howfastly.overrideAttrs {
+inputs.self.legacyPackages.${stdenv.hostPlatform.system}.crates.howfastly.overrideAttrs (old: {
   meta.mainProgram = "howfastly";
-}
+
+  # the dist is a trunk artifact rather than a crate output
+  # hang it off the cli so it stays out of the top level package set
+  passthru = (old.passthru or { }) // {
+    web = callPackage ./web.nix { inherit lib pkgsFinal; };
+  };
+})
